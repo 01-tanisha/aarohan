13f0502454daf9131b3b9adf51640833270cfa0b
01-tanisha <tanishasikarwar1@gmail.com>
2026-05-10 17:07:50 +0530

    new code

--- aarohan/wsgi.py
+++ aarohan/wsgi.py
@@ -8,16 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.wsgi import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aarohan.settings')
 
-try:
-    application = get_wsgi_application()
-    application = WhiteNoise(application)
-except Exception as e:
-    print(f"Error loading Django WSGI: {e}", file=sys.stderr)
-    raise
+application = get_wsgi_application()
